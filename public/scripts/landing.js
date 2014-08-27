8ed6f4816165d6b24a8c82c191b8608e5a51a765
Zach Smith <x.coder.zach@gmail.com>
2014-08-27 12:58:23 -0400

    woot

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -22,25 +22,24 @@
     dragging = true;
   })
 
-  headerDrag.on('end', function(){
+  headerDrag.on('end', function() {
     header.spring({ tension: 100, damping: 10 }).to(0, 0).start().then(function(){dragging = false})
   })
 
-  headerDrag2.on('end', function(){
+  headerDrag2.on('end', function() {
     header2.spring({ tension: 100, damping: 10 }).to(0, 0).start().then(function(){dragging = false})
   })
-
 
   var physicsEl = document.querySelector('.bounce')
   var physicsHeader = new Impulse(physicsEl)
     .style('translate', function(x, y)  { return x + 'px,' + y + 'px'})
 
   var bounce = function() {
-    if (!dragging)
+    if(!dragging)
       physicsHeader.spring().velocity(0, -250).from(0, 0).to(0, 0).start()
   }
 
-  bounce()
+  setTimeout(bounce, 1000)
 
   setInterval(bounce, 5000)
 })()
